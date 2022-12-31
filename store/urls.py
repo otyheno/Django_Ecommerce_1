6aed0fadbf0f1d9ec515abb74fb5fbb518956c01
Oty Henry <henryotieno66@gmail.com>
2022-12-31 18:25:03 +0300

    detail category page

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     #path('product/', views.productDetail, name='product_detail'),
-    path('<slug:slug>/', views.productDetail, name='product_detail'),
+    path('<slug:category_slug>/slug:slug>/', views.productDetail, name='product_detail'),
+    path('<slug:slug>/', views.categoryDetail, name='category_detail'),
 ]

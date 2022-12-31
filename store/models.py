a367baa372a1f663cc8e0ffdf1741abe96c8a65f
Oty Henry <henryotieno66@gmail.com>
2022-12-31 13:49:17 +0300

    created category model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)

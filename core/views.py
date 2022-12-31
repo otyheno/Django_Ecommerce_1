f2257755eb38a231454f9a13971b576437d854f6
Oty Henry <henryotieno66@gmail.com>
2022-12-31 16:42:27 +0300

    Listing new products on the front page

--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from store.models import Product
 
 # Create your views here.
 def frontPage(request):
-    return render(request, 'core/frontpage.html')
+    products = Product.objects.all()[0:6]
+
+    return render(request, 'core/frontpage.html', {'products': products})
 
 def aboutPage(request):
     return render(request, 'core/aboutpage.html')

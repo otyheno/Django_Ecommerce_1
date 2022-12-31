5cb5d2e116b52a779d3a1d3dbb9eea27c6640bcf
Oty Henry <henryotieno66@gmail.com>
2022-12-31 16:01:39 +0300

    Change Meta options on category     - Create model Product

--- store/models.py
+++ store/models.py
@@ -6,3 +6,22 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.title
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='products', on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+    description = models.TextField(max_length=300, blank=True, null=True)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title

575c0266ac88cbee3124e0a0a9da9ac30751f0e6
otyheno <henryotieno66@gmail.com>
2023-01-01 11:41:26 +0300

    modified product model to accomodate imagefield

--- store/models.py
+++ store/models.py
@@ -20,6 +20,7 @@ class Product(models.Model):
     slug = models.SlugField(max_length=50)
     description = models.TextField(max_length=300, blank=True, null=True)
     price = models.IntegerField()
+    image = models.ImageField(upload_to='uploads/product_images/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 

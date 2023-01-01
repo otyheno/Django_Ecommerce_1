d472fd7541576546253141b562ef47c1be3e479d
otyheno <henryotieno66@gmail.com>
2023-01-01 10:43:41 +0300

    fixed the error on category detail

--- store/views.py
+++ store/views.py
@@ -2,12 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
 # Create your views here.
-
-def productDetail(request, slug, category_slug):
-    #product = Product.objects.get(slug=slug)
-    product = get_object_or_404(Product, slug=slug)
-    return render(request, 'store/product_detail.html', {'product': product})
-
 def categoryDetail(request, slug):
     category = get_object_or_404(Category, slug=slug)
     return render(request, 'store/category_detail.html', {'category': category})
+
+def productDetail(request, category_slug, slug):
+    #product = Product.objects.get(slug=slug)
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, 'store/product_detail.html', {'product': product})

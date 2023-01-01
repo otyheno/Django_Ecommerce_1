0b61ea7a011532c79b95f491426a4c851e376220
otyheno <henryotieno66@gmail.com>
2023-01-01 11:23:19 +0300

    changed the url pattern to fix the error while accessing admin page

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -4,8 +4,8 @@ from django.urls import path, include
 from core.views import frontPage, aboutPage
 
 urlpatterns = [
-    path('', include('store.urls')),
-    path('', frontPage, name='frontpage'),
     path('about/', aboutPage, name='aboutpage'),
-    path('admin/', admin.site.urls),    
+    path('admin/', admin.site.urls),  
+    path('', include('store.urls')),
+    path('', frontPage, name='frontpage'),      
 ]

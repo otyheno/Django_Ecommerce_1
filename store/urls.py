3048fd08f022a9ad583a7e7e48b9ae464c8633ec
otyheno <henryotieno66@gmail.com>
2023-01-02 16:53:00 +0300

    simple search functionality

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     #path('product/', views.productDetail, name='product_detail'),
+    path('search/', views.search, name='search'),
     path('<slug:slug>/', views.categoryDetail, name='category_detail'),
     path('<slug:category_slug>/<slug:slug>/', views.productDetail, name='product_detail'),
     
--- store/views.py
+++ store/views.py
@@ -1,7 +1,17 @@
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
 # Create your views here.
+def search(request):
+    query = request.GET.get('query', '')
+    products = Product.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))
+    
+    return render(request, 'store/search.html', {
+        'query': query,
+        'products': products,
+        })
+    
 def categoryDetail(request, slug):
     category = get_object_or_404(Category, slug=slug)
     products = category.products.all()
@@ -14,3 +24,4 @@ def productDetail(request, category_slug, slug):
     #product = Product.objects.get(slug=slug)
     product = get_object_or_404(Product, slug=slug)
     return render(request, 'store/product_detail.html', {'product': product})
+
